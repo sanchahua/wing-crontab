e923bfcbf36b96d09e59ed249d99f90465695035
huangxiaoan <huangxiaoan@xunlei.com>
2018-05-08 20:49:24 +0800

    base develop

--- src/wing-crontab/main.go
+++ src/wing-crontab/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"app"
+	"library/path"
+	"controllers/consul"
+)
 
+func main() {
+	app.Init(path.CurrentPath + "/config")
+	defer app.Release()
+
+	ctx := app.NewContext()
+	control := consul.NewConsulController(ctx)
+	defer control.Close()
+
+	select {
+		case <- ctx.Done():
+	}
 }
