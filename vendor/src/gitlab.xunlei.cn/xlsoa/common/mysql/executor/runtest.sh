ab4046eb93bc3f6f2d9a92723501b72efce8821f
yuyi <297341015@qq.com>
2018-08-14 16:34:11 +0800

    update log

--- vendor/src/gitlab.xunlei.cn/xlsoa/common/mysql/executor/runtest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export MYSQL_TEST_HOST=10.10.159.17
-export MYSQL_TEST_PORT=3306
-export MYSQL_TEST_USER=root
-export MYSQL_TEST_PASSWORD="sd-9898w"
-export MYSQL_TEST_DBNAME=test
-
-go test -run Start -v
--- vendor/src/gitlab.xunlei.cn/xlsoa/config/example/README.md
+++
@@ -1,3 +0,0 @@
-```
-./main -addr "10.10.134.102:8599" -name "xlsoa-core-auth" #-force
-```
