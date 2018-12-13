8d19ff4d83ad8e43245a1c133273895d6ff240ac
xiaoan <huangxiaoan@xunlei.com>
2018-12-13 18:26:21 +0800

    add build script

--- bin/131.sh
+++ bin/131.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-export GOPATH=/home/yuyi/xllive && source  ~/.bash_profile && export GOROOT=/home/yuyi/go
+export GOPATH=/home/yuyi/xllive
+export PATH=/home/yuyi/go/bin:$PATH:$HOME/.local/bin:$HOME/bin
+export GOOGLEAPIS_DIR=/home/yuyi/xllive/src/googleapis
+export GOROOT=/home/yuyi/go
 ./build.sh
