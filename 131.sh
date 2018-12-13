15b1d249a54512fb35e9960c62c48552e8bc3e05
xiaoan <huangxiaoan@xunlei.com>
2018-12-13 18:12:57 +0800

    add build script

--- 131.sh
+++ 131.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export GOPATH=/home/yuyi/xllive && source  ~/.bash_profile && export GOROOT=/home/yuyi/xllive/src/gitlab.xunlei.cn/huangxiaoan/xcrontab/go
+export GOPATH=/home/yuyi/xllive && source  ~/.bash_profile && export GOROOT=/home/yuyi/go
 ./bin/build.sh
